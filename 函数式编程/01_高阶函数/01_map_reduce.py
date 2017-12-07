fa0f6994728a33a27d97062883212d2ed04b3e0c
huangkeke <hkkhuang@163.com>
2017-12-07 22:40:21 +0800

    Some of the operations of the file

--- 函数式编程/01_高阶函数/01_map_reduce.py
+++ 函数式编程/01_高阶函数/01_map_reduce.py
@@ -5,52 +5,51 @@
 # @Email   : hkkhuang@163.com
 # @File    : 01_map_reduce.py
 
-# # Python内建了map()和reduce()函数。
-# # map()函数接收两个参数，一个是函数，一个是Iterable，
-# # map将传入的函数依次作用到序列的每个元素，并把结果作为新的Iterator返回。
-#
-# # 举例说明，比如我们有一个函数f(x)=x2，
-# # 要把这个函数作用在一个list [1, 2, 3, 4, 5, 6, 7, 8, 9]上，就可以用map()实现如下：
-#
-#
-# def f(x):
-#     return x * x
-#
-#
-# result = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])
-# print(type(result))   # <class 'map'>
-# print(list(result))   # [1, 4, 9, 16, 25, 36, 49, 64, 81]
-#
-# # map()传入的第一个参数是f，即函数对象本身。
-# # 由于结果r是一个Iterator，Iterator是惰性序列，
-# # 因此通过list()函数让它把整个序列都计算出来并返回一个list。
+# Python内建了map()和reduce()函数。
+# map()函数接收两个参数，一个是函数，一个是Iterable，
+# map将传入的函数依次作用到序列的每个元素，并把结果作为新的Iterator返回。
 
-#
-# # map()作为高阶函数，事实上它把运算规则抽象了，
-# # 因此，我们不但可以计算简单的f(x)=x2，还可以计算任意复杂的函数，
-# # 比如，把这个list所有数字转为字符串：
-#
-# print(list(map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
-# # 执行结果
-# # ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-#
-# # reduce把一个函数作用在一个序列[x1, x2, x3, ...]上，
-# # 这个函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算
-# # 即：
-# # reduce(f, [x1, x2, x3, x4]) = f(f(f(x1, x2), x3), x4)
-#
-# from functools import reduce
-#
-#
-# def add(x, y):
-#     """reduce()练习，实现累加"""
-#     return x + y
-#
-#
-# r = reduce(add, [1, 2, 3, 4, 5, 6])
-# print(type(r))  # <class 'int'>
-# print(r)  # 21
+# 举例说明，比如我们有一个函数f(x)=x2，
+# 要把这个函数作用在一个list [1, 2, 3, 4, 5, 6, 7, 8, 9]上，就可以用map()实现如下：
 
+
+def f(x):
+    return x * x
+
+
+result = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(type(result))   # <class 'map'>
+print(list(result))   # [1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+# map()传入的第一个参数是f，即函数对象本身。
+# 由于结果r是一个Iterator，Iterator是惰性序列，
+# 因此通过list()函数让它把整个序列都计算出来并返回一个list。
+
+
+# map()作为高阶函数，事实上它把运算规则抽象了，
+# 因此，我们不但可以计算简单的f(x)=x2，还可以计算任意复杂的函数，
+# 比如，把这个list所有数字转为字符串：
+
+print(list(map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
+# 执行结果
+# ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+# reduce把一个函数作用在一个序列[x1, x2, x3, ...]上，
+# 这个函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算
+# 即：
+# reduce(f, [x1, x2, x3, x4]) = f(f(f(x1, x2), x3), x4)
+
+from functools import reduce
+
+
+def add(x, y):
+    """reduce()练习，实现累加"""
+    return x + y
+
+
+r = reduce(add, [1, 2, 3, 4, 5, 6])
+print(type(r))  # <class 'int'>
+print(r)  # 21
 
 from functools import reduce
 
@@ -69,33 +68,33 @@ print(r)  # 120
 from functools import reduce
 
 
-# def str2int(s):
-#     def fn(x, y):
-#         return x * 10 + y
-#
-#     def char2num(s):
-#         return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,  '9': 9}[s]
-#
-#     # print(list(map(char2num, s)))  # [1, 2, 3, 4, 5]
-#     return reduce(fn, map(char2num, s))
-#
-# # 考虑到字符串str也是一个序列
-# print(str2int('12345'))  # 12345
-#
-#
-# from functools import reduce
-#
-#
-# # 考虑到字符串str也是一个序列
-# def char2num(s):
-#     return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,  '9': 9}[s]
-#
-#
-# def str2int(s):
-#     return reduce(lambda x, y: x * 10 + y, map(char2num, s))
-#
-#
-# print(str2int('12345'))  # 12345
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+
+    def char2num(s):
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,  '9': 9}[s]
+
+    # print(list(map(char2num, s)))  # [1, 2, 3, 4, 5]
+    return reduce(fn, map(char2num, s))
+
+# 考虑到字符串str也是一个序列
+print(str2int('12345'))  # 12345
+
+
+from functools import reduce
+
+
+# 考虑到字符串str也是一个序列
+def char2num(s):
+    return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,  '9': 9}[s]
+
+
+def str2int(s):
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
+
+
+print(str2int('12345'))  # 12345
 
 
 # 练习

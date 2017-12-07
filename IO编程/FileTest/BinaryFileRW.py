e31b93042ba010aa8b60c7b5d0b84c2515f14087
huangkeke <hkkhuang@163.com>
2017-12-07 22:34:44 +0800

    Some of the operations of the file

--- IO编程/FileTest/BinaryFileRW.py
+++ IO编程/FileTest/BinaryFileRW.py
@@ -4,13 +4,41 @@
 # @Author  : huangkeke
 # @Email   : hkkhuang@163.com
 # @File    : BinaryFileRW.py
-
 """
-1.文本文件操作练习
-2.二进制文件的读写操作
+二进制文件的读写操作
 """
 
+import struct
+filename = "binary.data"
+# 写入文件
+with open(filename, 'wb') as file_write:
+    byte_i = struct.pack('i', 1000)  # 'i' 表示对int类型进行打包
+    file_write.write(byte_i)
 
+    byte_f = struct.pack('f', 1000.23)  # 'f' 表示对float类型进行打包
+    file_write.write(byte_f)
 
+    byte_d = struct.pack('d', 1000.456)  # 'd' 表示对double类型进行打包
+    file_write.write(byte_d)
+
+# 读取文件
+with open(filename, 'rb') as file_reader:
+    byte_len = 4
+    byte_i = file_reader.read(byte_len)  # 读取int类型的4个字节的二进制数据
+    print(struct.unpack('i', byte_i))  # 解压为原来int数据
+
+    byte_f = file_reader.read(byte_len)  # 读取float类型的4个字节的二进制数据
+    print(struct.unpack('f', byte_f))  # 解压为原来float数据
+
+    byte_len = 8
+    byte_d = file_reader.read(byte_len)  # 读取double类型的8个字节的二进制数据
+    print(struct.unpack('d', byte_d))  # 解压为原来double数据
+
+# 使用到struct进行打包成二进制字符串或者相应的解包成元组
+#
+# 注意: unpack返回的是tuple
+# (1000,)
+# (1000.22998046875,)
+# (1000.456,)
 
 
--- IO编程/FileTest/TextFileTest.py
+++ IO编程/FileTest/TextFileTest.py
@@ -10,10 +10,13 @@
 使用open()方法代开文件后一定要保证文件可以正常关闭
 所以我们对文件的操作使用try 应对可能会出现的异常
 在finally中将文件关闭，这样能够保证在正常或者出现一场的情况下都能正常将文件关闭
+
+注：不能把open语句放在try块里，因为当打开文件出现异常时，文件对象file_object无法执行close()方法。
 """
 
 # 解决 写入中文到文件乱码  在打开文件时候指定编码方式
 # file_object01 = open('test01.txt', 'w')  会出现乱码  需要指定编码方式
+# file_object01 = open('test01.txt', 'w', encoding='gbk')
 file_object01 = open('test01.txt', 'w', encoding='utf-8')
 try:
     all_the_text = file_object01.write('这是文本文件的读写操作')
@@ -23,6 +26,7 @@ finally:
 # 使用with 操作文件，这样Python会在我们不需要使用文件的时候自动将文件关闭
 # with open('test01.txt', 'r') as file_object02:  # 不制定编码方式会出现错误
 
+# with open('test01.txt', 'r', encoding='gbk') as file_object02:  # 写入和读取的编码要一致
 with open('test01.txt', 'r', encoding='utf-8') as file_object02:
     all_the_text = file_object02.read()
     print(all_the_text)
